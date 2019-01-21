bc0095c0328b4a8d1ed7f1416b23ff9576ed38da
OussamaAlouat <al317257@uji.es>
2019-01-21 18:45:27 +0100

    Added PostList component

---
+++ src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    render() {
+        return (
+            <div>Post List</div>
+        );
+    }
+
+}
+
+export default PostList;
