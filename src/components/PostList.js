4c918bf5c525e5ed96fd2f5c1faeaaefd07b9934
OussamaAlouat <al317257@uji.es>
2019-01-21 19:00:06 +0100

    Connect PostList component with actions

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from "../actions";
 
 class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>Post List</div>
@@ -9,4 +15,4 @@ class PostList extends React.Component {
 
 }
 
-export default PostList;
+export default connect(null, {fetchPosts})(PostList);
